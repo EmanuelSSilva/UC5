4548817e3f4ba593a59c1526c24fec3eaa40fc14
EMANUEL SILVA E SILVA <a95548885@rn.senac.local>
2025-10-20 20:57:49 -0300

    :feat: Desktop

--- Proj.Descktop/main.js
+++ Proj.Descktop/main.js
@@ -3,7 +3,8 @@ import {app, BrowserWindow} from 'electron';
 const criarJanela = () => {
     const janela = new BrowserWindow({
         width: 800, 
-        height: 600
+        height: 600,
+        title: "Minha Aplicação Electron"
     })
     
     janela.loadFile('index.html')
@@ -13,4 +14,13 @@ app.whenReady().then(() => {
     criarJanela()
     console.log("Aplicação iniciada com sucesso!")
 })
+.catch((erro) => {
+    console.error("Erro ao iniciar a aplicação:", erro)
+});
+
+app.on('window-all-closed', () => {
+    if (process.platform !== 'darwin') {
+        app.quit();
+    }
+});
 
